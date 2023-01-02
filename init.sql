fe56f9b6ac26ef7d1c1926e2053ea07917798751
gnituy18 <you@hsuyuting.com>
2023-01-02 22:34:08 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -4,7 +4,27 @@ CREATE TABLE IF NOT EXISTS users (
 	email varchar(320) UNIQUE,
 	avatar_url text,
 
-	google_id varchar(32) UNIQUE
+	google_id varchar(32) UNIQUE,
 
 	active boolean NOT NULL DEFAULT true,
+);
+
+CREATE TABLE IF NOT EXISTS politicians (
+	id SERIAL PRIMARY KEY,
+	name varchar(64) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS parties (
+	id int PRIMARY KEY,
+	name varchar(64) NOT NULL,
+	
+	chairman varchar(64),
+	established_date date,
+	filing_date date,
+
+	main_office_address text,
+	mailing_address text,
+
+	phone_number varchar(32),
+	status smallint NOT NULL DEFAULT 0,
 );

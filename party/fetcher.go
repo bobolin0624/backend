3de258bcd45ede8ae97b5ceee12a8910c52bea3e
gnituy18 <you@hsuyuting.com>
2023-01-05 02:16:12 +0800

    finish party download

--- party/fetcher.go
+++ party/fetcher.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"time"
 
+	"github.com/chromedp/cdproto/browser"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
 func main() {
+	//create a tmp folder for download
+	tmpDir, err := ioutil.TempDir("", "chromedp-")
+	if err != nil {
+		log.Fatal(err)
+	}
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
 	)
@@ -23,20 +30,38 @@ func main() {
 	var nodes []*cdp.Node
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://party.moi.gov.tw"),
-		chromedp.Nodes("#search_party", &nodes, chromedp.ByQuery),
+		chromedp.Nodes("search_party", &nodes, chromedp.ByID),
 	); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	n := nodes[0]
-	log.Println("nodes", n)
-	err := chromedp.Run(ctx,
-		chromedp.Nodes("#search_party", &nodes, chromedp.ByQuery),
+
+	done := make(chan struct{})
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if ev, ok := ev.(*browser.EventDownloadProgress); ok {
+			if ev.State == browser.DownloadProgressStateCompleted {
+				done <- struct{}{}
+				close(done)
+			}
+		}
+	})
+
+	var ns []*cdp.Node
+	if err := chromedp.Run(ctx,
 		chromedp.MouseClickNode(nodes[0]),
-		chromedp.Sleep(10*time.Second),
-		chromedp.WaitVisible(`#123`),
-	)
-	if err != nil {
-		log.Fatal(err)
+		chromedp.WaitVisible(`ContentPlaceHolder1_BTN_Export_Ods`, chromedp.ByID),
+		chromedp.Nodes("ContentPlaceHolder1_BTN_Export_Ods", &ns, chromedp.ByID),
+		browser.SetDownloadBehavior(browser.SetDownloadBehaviorBehaviorAllowAndName).WithDownloadPath(tmpDir+"/fff.ods").WithEventsEnabled(true),
+	); err != nil {
+		log.Println(err)
 	}
-	log.Printf("nodes", nodes)
+
+	if err := chromedp.Run(ctx,
+		chromedp.MouseClickNode(ns[0]),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			<-done
+			return nil
+		}),
+	); err != nil {
+		log.Println(err)
+	}
 }

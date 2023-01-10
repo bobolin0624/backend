f12c1adae51779abc9aea799dad68b08ac1c40e7
gnituy18 <you@hsuyuting.com>
2023-01-11 02:48:12 +0800

    Update staging data model

--- staging/model.go
+++ staging/model.go
@@ -3,9 +3,10 @@ package staging
 import "time"
 
 type StagingData struct {
-	GroupID   string
-	TableName string
-	Data      map[string]interface{}
+	GroupID   string                 `json:"groupId"`
+	TableName string                 `json:"tableName"`
+	Records   map[string]interface{} `json:"records"`
 
-	CreatedAt time.Time
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }

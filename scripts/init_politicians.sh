a77d982f153e13a069ade2b3057965c84cdf3a26
gnituy18 <you@hsuyuting.com>
2023-02-25 22:04:03 +0800

    Update mock politician scripts

--- scripts/init_politicians.sh
+++ scripts/init_politicians.sh
@@ -3,29 +3,29 @@ curl --location 'localhost:8080/politician' \
 --data '{
     "name": "王婉諭",
     "birthdate": "1979-04-26",
-    "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/4/48/%E7%AB%8B%E6%B3%95%E5%A7%94%E5%93%A1%E7%8E%8B%E5%A9%89%E8%AB%AD.jpg"
+    "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/4/48/%E7%AB%8B%E6%B3%95%E5%A7%94%E5%93%A1%E7%8E%8B%E5%A9%89%E8%AB%AD.jpg"
 }'
 
 curl --location 'localhost:8080/politician' \
 --header 'Content-Type: application/json' \
 --data '{
-    "name": "王婉諭",
-    "birthdate": "1979-04-26",
-    "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/4/48/%E7%AB%8B%E6%B3%95%E5%A7%94%E5%93%A1%E7%8E%8B%E5%A9%89%E8%AB%AD.jpg"
+    "name": "王世堅",
+    "birthdate": "1960-01-01",
+    "avatarUrl": "https://www.ly.gov.tw/Images/Legislators/ly1000_6_00003_23f.jpg"
 }'
 
 curl --location 'localhost:8080/politician' \
 --header 'Content-Type: application/json' \
 --data '{
-    "name": "高嘉瑜",
-    "birthdate": "1980-10-17",
-    "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/1/17/%E7%AB%8B%E6%B3%95%E5%A7%94%E5%93%A1%E9%AB%98%E5%98%89%E7%91%9C.jpg"
+    "name": "許淑華",
+    "birthdate": "1975-10-15",
+    "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/e/e7/%E8%A8%B1.JPG"
 }'
 
 curl --location 'localhost:8080/politician' \
 --header 'Content-Type: application/json' \
 --data '{
-    "name": "高嘉瑜",
-    "birthdate": "1980-01-01",
-    "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/1/17/%E7%AB%8B%E6%B3%95%E5%A7%94%E5%93%A1%E9%AB%98%E5%98%89%E7%91%9C.jpg"
+    "name": "許淑華",
+    "birthdate": "1973-05-22",
+    "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c4/Hsu_Shu-Hua_at_World_Design_Capital_Taipei_press_conference_20120629.jpg"
 }'

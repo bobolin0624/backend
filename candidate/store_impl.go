05393c69b75e3337050bd64687ab28bc2e4bd2b2
gnituy18 <you@hsuyuting.com>
2023-02-26 23:41:48 +0800

    Fix create candidate

--- candidate/store_impl.go
+++ candidate/store_impl.go
@@ -2,6 +2,7 @@ package candidate
 
 import (
 	"context"
+	"log"
 
 	"github.com/taiwan-voting-guide/backend/model"
 	"github.com/taiwan-voting-guide/backend/pg"
@@ -21,10 +22,11 @@ func (im *impl) Create(ctx context.Context, candidate *model.Candidate) error {
 	}
 	defer conn.Close(ctx)
 
-	if _, err := conn.Query(ctx, `
+	if _, err := conn.Exec(ctx, `
 		INSERT INTO candidates (type, term, politician_id, number, elected, party_id, area, vice_president)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 	`, candidate.Type, candidate.Term, candidate.PoliticianId, candidate.Number, candidate.Elected, candidate.PartyId, candidate.Area, candidate.VicePresident); err != nil {
+		log.Panicln(err)
 		return err
 	}
 

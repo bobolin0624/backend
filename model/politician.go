bcfe2a652ccc5e982fe13d1d950bc62a16ed2e5c
bobolin0624 <bobolincowo@gmail.com>
2023-03-30 19:22:24 +0800

    chore: format

--- model/politician.go
+++ model/politician.go
@@ -5,37 +5,37 @@ import (
 )
 
 type Politician struct {
-	Id        int
-	Name      string
-	Birthdate time.Time
-	AvatarUrl *string
-	Sex       Sex
-	Meta      []byte
+	Id             int
+	Name           string
+	Birthdate      time.Time
+	AvatarUrl      *string
+	Sex            Sex
+	Meta           []byte
 	CurrentPartyId *int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
 func (p *Politician) Repr() *PoliticianRepr {
 	return &PoliticianRepr{
-		Name:      p.Name,
-		Birthdate: p.Birthdate,
-		AvatarUrl: p.AvatarUrl,
-		Sex:       p.Sex,
+		Name:           p.Name,
+		Birthdate:      p.Birthdate,
+		AvatarUrl:      p.AvatarUrl,
+		Sex:            p.Sex,
 		CurrentPartyId: p.CurrentPartyId,
-		CreatedAt: p.CreatedAt.Unix(),
-		UpdatedAt: p.UpdatedAt.Unix(),
+		CreatedAt:      p.CreatedAt.Unix(),
+		UpdatedAt:      p.UpdatedAt.Unix(),
 	}
 }
 
 type PoliticianRepr struct {
-	Name      string    `json:"name"`
-	Birthdate time.Time `json:"birthdate"`
-	AvatarUrl *string    `json:"avatarUrl"`
-	Sex       Sex       `json:"sex"`
-	CurrentPartyId *int `json:"currentPartyId"`
-	CreatedAt int64 `json:"createdAt"`
-	UpdatedAt int64 `json:"updatedAt"`
+	Name           string    `json:"name"`
+	Birthdate      time.Time `json:"birthdate"`
+	AvatarUrl      *string   `json:"avatarUrl"`
+	Sex            Sex       `json:"sex"`
+	CurrentPartyId *int      `json:"currentPartyId"`
+	CreatedAt      int64     `json:"createdAt"`
+	UpdatedAt      int64     `json:"updatedAt"`
 }
 
 type Sex string
